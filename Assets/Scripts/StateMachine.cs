456ff72f8c7e38dffb6e44b1d93920592b9da72a
Lucas Leonel Siqueira <lucasleosiq@gmail.com>
2023-03-08 08:46:30 -0300

    refactor: create a class for the state machine

--- Assets/Scripts/StateMachine.cs
+++ Assets/Scripts/StateMachine.cs
@@ -1,50 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using NaughtyAttributes;
 
-public class StateMachine : MonoBehaviour
+public class Test
 {
-    public enum States
+    public enum Test2
     {
-        NONE,
+        NONE
     }
 
-    public Dictionary<States, StateBase> dictionaryState;
+    public void Aa()
+    {
+        StateMachine<Test2> stateMachine = new StateMachine<Test2>();
+
+        stateMachine.RegisterStates(Test.Test2.NONE, new StateBase());
+    }
+}
+
+public class StateMachine<T> where T : System.Enum
+{
+    public Dictionary<T, StateBase> dictionaryState;
 
     private StateBase _currentState;
     public float timeToStartGame = 1f;
 
-    private void Awake()
+    public void Init()
     {
-        dictionaryState = new Dictionary<States, StateBase>();
-        dictionaryState.Add(States.NONE, new StateBase());
-
-        SwitchState(States.NONE);
-
-        Invoke(nameof(StartGame), timeToStartGame);
+        dictionaryState = new Dictionary<T, StateBase>();
+    }
+    
+    public void RegisterStates(T typeEnum, StateBase state)
+    {
+        dictionaryState.Add(typeEnum, state);
     }
 
-    private void StartGame()
+    public void SwitchState(T state)
     {
-        SwitchState(States.NONE);
-    }
-
-    private void SwitchState(States state)
-    {
-        if (_currentState != null) _currentState.OnStateExit();
+        if (_currentState != null) _currentState.OnStateExit(); 
 
         _currentState = dictionaryState[state];
 
         _currentState.OnStateEnter();
     }
 
-    private void Update()
+    public void Update()
     {
         if (_currentState != null) _currentState.OnStateStay();
-
-        if(Input.GetKeyDown(KeyCode.O))
-        {
-            //SwitchState(States.DEAD);
-        }
     }
 }
