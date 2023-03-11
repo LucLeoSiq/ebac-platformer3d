cc0bab8ea2e2c68f5b79d7a79c4985c97a41dac9
Lucas Leonel Siqueira <lucasleosiq@gmail.com>
2023-03-11 19:26:55 -0300

    refactor: remove unused libraries

--- Assets/Scripts/GameManager/GMStateIntro.cs
+++ Assets/Scripts/GameManager/GMStateIntro.cs
@@ -1,8 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using Ebac.StateMachine;
 
-public class GMStateIntro : MonoBehaviour
+public class GMStateIntro : StateBase
 {
 
 }
