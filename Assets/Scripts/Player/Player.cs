45ea0d4b3070f44ddbfed0c9f2b225fe22542e90
Lucas Leonel Siqueira <lucasleosiq@gmail.com>
2023-03-14 08:48:27 -0300

    feat: add movement and rotation to player

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,9 +6,17 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public CharacterController characterController;
     public float speed = 1f;
+    public float turnSpeed = 1f;
+
     void Update()
     {
-        var val = transform.forward * Input.GetAxis("Vertical") * speed;
+        transform.Rotate(0, Input.GetAxis("Horizontal") * turnSpeed* Time.deltaTime, 0);
+        
+        // Makes object which script is attached move vertically.
+        var speedVector = transform.forward * Input.GetAxis("Vertical") * speed;
+
+        characterController.Move(speedVector * Time.deltaTime);
     }
 }
