b3dd207e7542fc6943ac8bb3b89bf97d622938fd
Lucas Leonel Siqueira <lucasleosiq@gmail.com>
2023-03-28 18:56:32 -0300

    feat: add particle effects to enemy when hit

--- Assets/Scripts/Enemies/EnemyBase.cs
+++ Assets/Scripts/Enemies/EnemyBase.cs
@@ -6,6 +6,9 @@ using UnityEngine;
 public class EnemyBase : MonoBehaviour, IDamageable
 {
     public Collider collider;
+    public FlashColor flashColor;
+    public ParticleSystem particleSystem;
+
     public float startLife = 10f;
 
     [SerializeField] private float _currentLife;
@@ -50,6 +53,9 @@ public class EnemyBase : MonoBehaviour, IDamageable
 
     public void OnDamage(float f)
     {
+        if (flashColor != null) flashColor.Flash();
+        if (particleSystem != null) particleSystem.Emit(15);
+
         _currentLife -= f;
         
         if(_currentLife <= 0)
