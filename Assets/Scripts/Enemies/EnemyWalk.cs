826696279ea8b8b3f2a1dd17b6c639b0fe1b7327
Lucas Leonel Siqueira <lucasleosiq@gmail.com>
2023-06-15 09:06:53 -0300

    chore: remove unused imports

--- Assets/Scripts/Enemies/EnemyWalk.cs
+++ Assets/Scripts/Enemies/EnemyWalk.cs
@@ -1,6 +1,3 @@
-using Cinemachine.Editor;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyWalk : EnemyBase
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,8 +1,5 @@
-using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
-using UnityEditor.U2D;
 using UnityEngine;
 using Ebac.Core.Singleton;
 using Cloth;
